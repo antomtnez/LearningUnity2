0ee8b81146ef13bee4dcd24a58c3eaae63911259
Anto Martínez <antomartinezdev@gmail.com>
2024-03-11 18:03:02 +0100

    Unit 3 - I add player animator component to control jump and run animations with the actions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,26 +2,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float jumpForce = 100f;
+    public float jumpForce = 10f;
     public float gravityModifier;
+    private bool isOnGround = false;
 
     private Rigidbody _rb;
-    private bool isOnGround = false;
+    private Animator _playerAnimator;
 
     public bool gameOver = false;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _playerAnimator = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             Jump();
             isOnGround = false;
+            _playerAnimator.SetTrigger("Jump_trig");
         }
     }
 
@@ -36,7 +39,12 @@ public class PlayerController : MonoBehaviour
             isOnGround = true;
 
         if(other.gameObject.CompareTag("Obstacle"))
+        {
             gameOver = true;
             Debug.Log("Game Over!");    
+            _playerAnimator.SetBool("Death_b", true);
+            _playerAnimator.SetInteger("DeathType_int", 1);
+        }
+            
     }
 }
